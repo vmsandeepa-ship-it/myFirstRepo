641cf7ba3afebd3e112c0e064ff33b7cd0c6ba3a
sandeepavm <vmsandeepa@gmail.com>
2026-02-06 20:21:50 +0530

    modified the file

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 def calculate(a, b):
-    return a + b
+    return a * b
 
 # this is a calculator function
+print(calculate(5, 10))
+
+# git log, git log --oneline, git status, git diff, git clone, git branch, git switch, git stash
